4d0125a54bcadbcaa2aac9234fc691e36e9cdcd1
ATCC 楊　秋陽 <qiuyang.yang@accenture.com>
2023-03-22 17:23:21 +0900

    二つ目

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello yqycicd!, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello　二つ目yqycicd!, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
